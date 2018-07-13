4fe724eefb328ae4d48e9ae692698f4064935568
david aguilo <david.aguilo232@gmail.com>
2018-07-13 01:29:46 -0400

    Listo para Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'pages/index'
   resources :students
+  root'students#index'
+  get 'pages/landing'
+
+  #root'pages#landing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
